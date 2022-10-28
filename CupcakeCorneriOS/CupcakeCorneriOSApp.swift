9ca95cf8f1b371c3f7184b3ffe98c35f174db0c9
Alison Gorman <alison@minimascientific.com>
2022-10-28 14:29:19 -0600

    initial commit

---
+++ CupcakeCorneriOS/CupcakeCorneriOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorneriOSApp.swift
+//  CupcakeCorneriOS
+//
+//  Created by Alison Gorman on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorneriOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
